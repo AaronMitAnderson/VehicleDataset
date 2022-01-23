3802c90c638625a0392a23bab68d7c4032abff36
Aaron Anderson <aaronmitanderson@gmail.com>
2022-01-23 15:55:53 -0500

    Added Print to CSV

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib3 import Timeout
+import csv
 
 def get_carmax_page_api():
   """Get data for a page of 20 vehicles from the CarMax API."""
   # make a GET request to the vehicles endpoint
   # page_url = 'https://api.carmax.com/v1/api/vehicles?apikey=adfb3ba2-b212-411e-89e1-35adab91b600'
-  numResultsShown = '20'
-  storeID = '7197'
+  numResultsShown = '100'
   zipCode = '28226'
-  searchText = 'Truck'
+  # searchText = 'Truck'
   distance = '1'
-  maxMilage = '80000'
+  # maxMilage = '80000'
 
-  # DEPRECIATED
-  # ________________________
-  # # page_url = f'https://shoppersapp-gateway.carmax.com/api/search/results?Take={numResultsShown}&UserStoreId={storeID}&StoreId={storeID}&ZipCode={zipCode}&SearchText={searchText}&Distance={distance}&MileageMax={maxMilage}'
-  # page_url = "https://www.carmax.com/cars/api/search/run?uri=%2Fcars%3Fsearch%3DTruck&skip=0&take=48&zipCode=28226&radius=40&shipping=0&sort=20&scoringProfile=BestMatchScoreVariant3&visitorID=6ec4db73-f249-497a-9735-48ca92858ffd"
-  # print(page_url)
-  # response = requests.get(page_url)
-  # print("Response Received")
-  # ________________________
-
-  page_url = f'https://www.carmax.com/cars/api/search/run?uri=%2Fcars%3Fsearch%3DTruck&skip=0&take=100&zipCode=28226&radius=40&shipping=0&sort=20'
+  page_url = f'https://www.carmax.com/cars/api/search/run?uri=%2Fcars%3Fsearch%3DTruck&skip=0&take={numResultsShown}&zipCode={zipCode}&radius={distance}&shipping=0&sort=20'
 
   payload={}
   headers = {'Accept': '*/*',
@@ -47,52 +37,47 @@ def get_carmax_page_api():
     print('Request Timeout')
   else:
     print("Response Received")
+    num_results = response.json()['totalCount']
     # print(response.text)
 
+  print()
   # get JSON from requests and return the results subsection
   # print(response.json()['results'][0:]['stockNumber'])
   listOfStockNumbers = []
   vehicles = response.json()['items']
   for vehicle in vehicles:
-    if vehicle['isTransferable'] == True:
+    if vehicle['isTransferable'] == True or vehicle['transferText'] == "Available at your store":
       listOfStockNumbers.append(vehicle['stockNumber'])
     
   print(listOfStockNumbers)
-  # for stockNumber in listOfStockNumbers:
-  #   print(stockNumber)
-  return listOfStockNumbers
+  return listOfStockNumbers, vehicles
 
 
-def getDictofCars(listOfStock):
+def getDictofCars(listOfStock, vehicles):
   listOfCars = []
-  # page_url = f'https://shoppersapp-gateway.carmax.com/api/vehicles/21403097'
-  # response = requests.get(page_url)
-  # result = response.json()['vehicle']
-  # price = response.json()['price']
-  # sN = result['stockNumber']
-  # carDict = {"stockNumber": result['stockNumber'],
-  #            "type" : result['description'],
-  #            "price" : price['displayPrice'],
-  #            "url" : f"https://www.carmax.com/car/{sN}"}
-  # print(carDict)
 
-  for stockNumber in listOfStock:
-    carDict = {}
-    page_url = f'https://shoppersapp-gateway.carmax.com/api/vehicles/{stockNumber}'
-    response = requests.get(page_url)
-    result = response.json()['vehicle']
-    price = response.json()['price']
-    sN = result['stockNumber']
-    carDict = {"stockNumber": result['stockNumber'],
-              "price" : price['displayPrice'],
-              "type" : result['description'],
-              "location" : result['storeName'],
-              "url" : f"https://www.carmax.com/car/{sN}"}
+  for vehicle in vehicles:
+    Type = f"{vehicle['year']} {vehicle['make']} {vehicle['model']} {vehicle['trim']}"
+    carDict = {"StockNumber": vehicle['stockNumber'],
+               "Price" : vehicle['basePrice'],
+               "MSRP" : vehicle['msrp'],
+               "Markdown" : vehicle['currentMarkdown'],
+               "Type" : Type,
+               "Drive Train" : vehicle['driveTrain'],
+               "Location" : vehicle['storeName']+ " " + vehicle['storeCity'],
+               "URL" : f"https://www.carmax.com/car/{vehicle['stockNumber']}"}
     listOfCars.append(carDict)
   
+  field_names= ['StockNumber', 'Price', 'MSRP', 'Markdown', 'Type', 'Drive Train', 'Location', 'URL']
+
+  with open('Cars.csv', 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=field_names, lineterminator='\n')
+    writer.writeheader()
+    writer.writerows(listOfCars)
+
   erroneousCars = []
   for car in listOfCars:
-    if car['price'] <= 30000:
+    if car['Price'] <= 30000:
       erroneousCars.append(car)
     print(car)
 
@@ -105,9 +90,8 @@ def getDictofCars(listOfStock):
       print(misprice)
 
 def main():
-  listOfStock = get_carmax_page_api()
-  print()
-  getDictofCars(listOfStock)
+  listOfStock, vehicles = get_carmax_page_api()
+  getDictofCars(listOfStock, vehicles)
 
 if __name__ == "__main__":
   main()
